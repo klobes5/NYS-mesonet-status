baf5a008c93f3b6fe9bf7b635f52103abbcee0d1
Kevin LoBiondo <kevin.lobiondo@gmail.com>
2016-09-09 01:15:18 +0000

    Modified query

--- query.sql
+++ query.sql
@@ -1,1 +1,2 @@
-select stationid, lastReportTime from SiteStatusHistory_dev.StationReport_tbl WHERE lastReportTime BETWEEN '2016-09-01 00:00:00' and '2016-09-07 00:00:00' ORDER BY stationid
+select stationid, lastReportTime,ingestid from SiteStatusHistory_dev.StationReport_tbl WHERE lastReportTime BETWEEN '2016-09-01 00:00:00' and '2016-09-07 00:00:00' ORDER BY stationid, lastReportTime DESC
+
