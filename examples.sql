34c89333ffc3a0173666168b21ef5bfd0c750265
Kevin LoBiondo <kevin.lobiondo@gmail.com>
2016-09-21 19:10:22 +0000

    Removed unnecessary files.

--- examples.sql
+++
@@ -1,4 +0,0 @@
-select stationId, lastReportTime, count(*) as count
-from SiteStatusHistory_dev.StationReport_tbl 
-WHERE lastReportTime BETWEEN '2016-09-13 00:00:00' and '2016-09-13 00:30:00' 
-Group BY stationId, UNIX_TIMESTAMP(lastReportTime) DIV 300 ORDER BY stationId ASC
--- query.sql
+++
@@ -1,2 +0,0 @@
-select stationid, lastReportTime,ingestid from SiteStatusHistory_dev.StationReport_tbl WHERE lastReportTime BETWEEN '2016-09-01 00:00:00' and '2016-09-07 00:00:00' ORDER BY stationid, lastReportTime DESC
-
